b4a8ed014d7d75ece71a2856d3ad94c3483b9ee2
Poojasree Keerthipati <51318864+poopati@users.noreply.github.com>
2025-02-06 13:09:17 -0600

    Update main.py
    updated UI

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from prediction_helper import predict
-st.title('Health Insurance Premium Estimator')
 
+st.set_page_config(layout="wide")
+
+col1, col2, col3 = st.columns([1, 2, 1])  # Wider middle column
+with col2:
+    st.title("Health Insurance Premium Estimator")
 
 categorical_options = {
     'Gender': ['Male', 'Female'],
@@ -22,44 +26,47 @@ categorical_options = {
 
 # Create four rows of three columns each
 
-row1 = st.columns(3)
-row2 = st.columns(3)
-row3 = st.columns(3)
-row4 = st.columns(3)
-row5 = st.columns(3)
+row1 = st.columns(6)
+row2 = st.columns(6)
+row3 = st.columns(6)
+row4 = st.columns(6)
+row5 = st.columns(6)
+row6 = st.columns([1,2,1,1,1])
 
 
 # Assign inputs to the grid
-with row1[0]:
-    age = st.number_input('Age', min_value=18, step= 1, max_value=100)
 with row1[1]:
-    number_of_dependants = st.number_input('Number of Dependants', min_value= 0 , step =1, max_value=20)
+    age = st.number_input('Age', min_value=18, step= 1, max_value=100)
 with row1[2]:
-    income_level = st.selectbox('Income Level', categorical_options['Income Level'])
-
-with row2[0]:    
-    income_lakhs = st.number_input('Income Lakhs', min_value= 0 , step =1, max_value=200)
-with row2[1]:
-    genetical_risk = st.number_input('Genetical Risk', min_value=0, step= 1, max_value=5)
-with row2[2]:
-    insurance_plan = st.selectbox('Insurance Plan', categorical_options['Insurance Plan'])
-
-with row3[0]:
     employment_status = st.selectbox('Employment Status', categorical_options['Employment Status'])
-with row3[1]:
-    gender= st.selectbox('Gender', categorical_options['Gender'])
-with row3[2]:
-    marital_status = st.selectbox('Marital Status', categorical_options['Marital Status'])
+with row1[3]:
+    income_level = st.selectbox('Income Level', categorical_options['Income Level'])
+with row1[4]:    
+    income_lakhs = st.number_input('Income in Lakhs', min_value= 0 , step =1, max_value=200)
 
-with row4[0]:
-    bmi_category = st.selectbox('BMI Category', categorical_options['BMI Category'])
-with row4[1]:
-    smoking_status= st.selectbox('Smoking Status', categorical_options['Smoking Status'])
-with row4[2]:
+with row2[1]:
+    gender= st.selectbox('Gender', categorical_options['Gender'])
+with row2[2]:
+    marital_status = st.selectbox('Marital Status', categorical_options['Marital Status']) 
+with row2[3]:
+    number_of_dependants = st.number_input('Number of Dependants', min_value= 0 , step =1, max_value=20)
+with row2[4]:
     region = st.selectbox('Region', categorical_options['Region'])
 
-with row5[0]:
+
+with row3[1]:
+    bmi_category = st.selectbox('BMI Category', categorical_options['BMI Category'])
+with row3[2]:
+    smoking_status= st.selectbox('Smoking Status', categorical_options['Smoking Status'])
+with row3[3]:
+    genetical_risk = st.number_input('Genetical Risk', min_value=0, step= 1, max_value=5)
+with row3[4]:
     medical_history = st.selectbox('Medical History', categorical_options['Medical History'])
+
+with row4[1]:
+    insurance_plan = st.selectbox('Insurance Plan', categorical_options['Insurance Plan'])
+
+
 
 # create a dictionary for input values
 input_dict = {
@@ -79,7 +86,7 @@ input_dict = {
 }
 
 # Button to make prediction
-if st.button('Predict'):
-    prediction = predict(input_dict)
-    st.success(f'The estimated Health Insurance Cost is: {round(prediction[0])} INR')
-
+with row6[1]:
+    if st.button('Predict'):
+        prediction = predict(input_dict)
+        st.success(f'The estimated Health Insurance Cost is: {round(prediction[0])} INR')
